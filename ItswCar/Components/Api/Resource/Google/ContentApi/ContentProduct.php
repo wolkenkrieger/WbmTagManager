436deae9e0fe259fd4608cf846cda243a2a2090d
wolkenkrieger <wolkenkrieger@gmail.com>
2021-08-24 09:03:07 +0200

    .

--- ItswCar/Components/Api/Resource/Google/ContentApi/ContentProduct.php
+++ ItswCar/Components/Api/Resource/Google/ContentApi/ContentProduct.php
@@ -221,6 +221,6 @@ class ContentProduct {
 	 * @return float
 	 */
 	private function getPriceFactor(): float {
-		return (float)rand() / (float)getrandmax() + (float)rand(1, 2);
+		return (float)rand() / (float)getrandmax() + 1.1111;
 	}
 }
