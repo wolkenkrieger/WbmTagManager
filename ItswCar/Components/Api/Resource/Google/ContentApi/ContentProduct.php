0942fef31230a2347ee761529352290680531b60
wolkenkrieger <wolkenkrieger@gmail.com>
2021-09-06 07:34:01 +0200

    .

--- ItswCar/Components/Api/Resource/Google/ContentApi/ContentProduct.php
+++ ItswCar/Components/Api/Resource/Google/ContentApi/ContentProduct.php
@@ -63,7 +63,7 @@ class ContentProduct {
 			$productImageUrls[] = $this->mediaService->getUrl($image->getMedia()->getPath());
 		}
 		
-		$productAvailability = $this->product->getMainDetail()->getInStock()? 'auf_Lager' : 'nicht_auf_Lager';
+		$productAvailability = $this->product->getMainDetail()->getInStock()? 'auf Lager' : 'nicht auf Lager';
 		
 		$productPrice = 0;
 		$discount = 0;
