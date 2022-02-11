ef8c11844d1195367e1d02bc71976d057bdb6e24
wolkenkrieger <wolkenkrieger@gmail.com>
2022-02-11 09:23:08 +0100

    2.2.6

--- ItswCar/Components/Google/ContentApi/ContentProduct.php
+++ ItswCar/Components/Google/ContentApi/ContentProduct.php
@@ -194,7 +194,8 @@ class ContentProduct {
 			foreach($shippingInfos as $shippingInfo) {
 				if (in_array($shippingInfo['countryISO'], self::TARGET_COUNTRIES, TRUE)) {
 					$shippingPrice = new Google_Service_ShoppingContent_Price();
-					$shippingPrice->setValue(number_format((float)$shippingInfo['shippingCost'], 2, ',', '.'));
+					//$shippingPrice->setValue(number_format((float)$shippingInfo['shippingCost'], 2, ',', '.'));
+					$shippingPrice->setValue((string)$shippingInfo['shippingCost']);
 					$shippingPrice->setCurrency('EUR');
 					
 					$shipping = new Google_Service_ShoppingContent_ProductShipping();
@@ -207,7 +208,8 @@ class ContentProduct {
 			}
 		} else {
 			$shippingPrice = new Google_Service_ShoppingContent_Price();
-			$shippingPrice->setValue('0,00');
+			//$shippingPrice->setValue(number_format((float)$shippingInfo['shippingCost'], 2, ',', '.'));
+			$shippingPrice->setValue('0.00');
 			$shippingPrice->setCurrency('EUR');
 			
 			$shipping = new Google_Service_ShoppingContent_ProductShipping();
