7cdd5807bb1aa7a3880966e3b973797505ce8d99
wolkenkrieger <wolkenkrieger@gmail.com>
2022-06-27 09:03:51 +0200

    .

--- ItswCar/Components/Google/ContentApi/ContentProduct.php
+++ ItswCar/Components/Google/ContentApi/ContentProduct.php
@@ -138,6 +138,13 @@ class ContentProduct {
 		
 		//$fakePrice = $productPrice * $this->getPriceFactor();
 		//$fakePrice = $this->product->getMainDetail()->getAttribute()->getFakePrice()?:$productPrice;
+		$description = $this->product->getDescriptionLong();
+		
+		try {
+			$description = $this->productHelper->fixDescription($description, $this->product->getName());
+		} catch (\Exception $exception) {
+			$this->error($exception);
+		}
 		
 		$description = str_ireplace([
 			'</ul><br><br><div id="description_oe">',
@@ -146,13 +153,9 @@ class ContentProduct {
 			'<li>',
 			'</li></ul>'
 		],
-			$this->textHelper->filterBadWords($this->product->getDescriptionLong()));
+			$this->textHelper->filterBadWords($description));
 		
-		try {
-			$description = $this->productHelper->fixDescription($description, $this->product->getName());
-		} catch (\Exception $exception) {
-			$this->error($exception);
-		}
+		
 		
 		$productMpn = $this->product->getMainDetail()->getSupplierNumber()? : 'ATW-'.$this->product->getMainDetail()->getId();
 		
